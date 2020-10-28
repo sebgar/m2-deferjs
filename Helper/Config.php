6f3e17bd617e104ccdb45adb18b675f90029209c
sgarrouste <sgarrouste@datasolution.fr>
2020-10-28 16:04:29 +0100

    deferjs : dev

---
+++ Helper/Config.php
@@ -0,0 +1,19 @@
+<?php
+namespace Sga\DeferJs\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Config extends AbstractHelper
+{
+    const XML_PATH_ENABLED = 'dev/js/defer_enabled';
+
+    public function isEnabled($store = null)
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLED,
+            ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+}
---
+++ Observer/BeforeSendResponse.php
@@ -0,0 +1,74 @@
+<?php
+namespace Sga\DeferJs\Observer;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\State as AppState;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Sga\DeferJs\Helper\Config as ConfigHelper;
+
+class BeforeSendResponse implements ObserverInterface
+{
+    protected $_configHelper;
+    protected $_appState;
+    protected $_scopeConfig;
+
+    public function __construct(
+        ConfigHelper $configHelper,
+        AppState $appState,
+        ScopeConfigInterface $scopeConfig
+    ){
+        $this->_configHelper = $configHelper;
+        $this->_appState = $appState;
+        $this->_scopeConfig = $scopeConfig;
+    }
+
+    public function execute(Observer $observer)
+    {
+        if ($this->_appState->getAreaCode() === 'frontend' && $this->_configHelper->isEnabled() && !isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+            $response = $observer->getEvent()->getData('response');
+            if ($response) {
+                $html = $response->getBody();
+
+                $conditionalJsPattern = '@(?:<script(.*)>)(.*)</script>@msU';
+                preg_match_all($conditionalJsPattern, $html, $matches);
+
+                $patterns = $this->_getPatterns();
+                foreach ($matches[0] as $i => $js) {
+                    $jsDefer = '<script defer="defer" '.$matches[1][$i].'>';
+
+                    $foundPattern = false;
+                    foreach ($patterns as $pattern) {
+                        if (preg_match($pattern, $matches[2][$i])) {
+                            $foundPattern = true;
+                            $jsDefer .= 'document.addEventListener("DOMContentLoaded", function() {'.$matches[2][$i].'});';
+                            break;
+                        }
+                    }
+
+                    if (!$foundPattern) {
+                        $jsDefer .= $matches[2][$i];
+                    }
+
+                    $jsDefer .= '</script>';
+
+                    $html = str_replace($js, $jsDefer, $html);
+                }
+
+                $response->setBody($html);
+            }
+        }
+    }
+
+    protected function _getPatterns()
+    {
+        $list = [];
+        $nodes = $this->_scopeConfig->get('system', 'default/dev/js/patterns');
+        if (is_array($nodes)) {
+            foreach ($nodes as $key => $node) {
+                $list[] = $node;
+            }
+        }
+        return $list;
+    }
+}
